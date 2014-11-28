3ec792292291229ae44a23c160a80411b15ade6a
sakeven <jc5930@sina.cn>
2014-11-28 23:01:32 +0800

    delete function

--- tpl.go
+++ tpl.go
@@ -1,7 +1,10 @@
 package restweb
 
 import (
+	"fmt"
 	"html/template"
+	"strings"
+	"time"
 )
 
 var funcMap map[string]interface{}
@@ -25,4 +28,49 @@ func initFuncMap() {
 	funcMap["NumSub"] = NumSub
 	funcMap["ShowTime"] = ShowTime
 	funcMap["ShowGapTime"] = ShowGapTime
+}
+
+// ShowNext 返回num加1的值
+func ShowNext(num int) (next int) {
+	next = num + 1
+	return
+}
+
+// ShowTime 将unixtime转换为当地时间
+func ShowTime(unixtime int64) string {
+	return time.Unix(unixtime, 0).Local().Format("2006-01-02 15:04:05")
+}
+
+// NumAdd 将两数相加
+func NumAdd(a int, b int) (ret int) {
+	ret = a + b
+	return
+}
+
+// NumSub 两数相减a-b
+func NumSub(a int, b int) (ret int) {
+	ret = a - b
+	return
+}
+
+// 格式化间隔时间
+func ShowGapTime(gaptime int64) string {
+	sec := gaptime % 60
+	hour := gaptime / 3600
+	minute := (gaptime - hour*3600) / 60
+	return fmt.Sprintf("%d:%02d:%02d", hour, minute, sec)
+}
+
+func GetTime() (t int64) {
+	t = time.Now().Unix()
+	return
+}
+
+func GetAction(path string, pos int) string {
+	path = strings.Trim(path, "/")
+	pathsplit := strings.Split(path, "/")
+	if pos >= 0 && pos < len(pathsplit) {
+		return pathsplit[pos]
+	}
+	return ""
 }
